c644a41668ab19ec8e44338153d491a59c41a56b
qiyun.yqy <qiyun.yqy@gmail.com>
2020-06-18 03:08:06 +0800

    给快排添加注释

--- src/main/java/com/github/yqy7/miscdemo/QSort.java
+++ src/main/java/com/github/yqy7/miscdemo/QSort.java
@@ -4,17 +4,16 @@ import java.util.Arrays;
 
 public class QSort {
     public void sort(int[] arr, int start, int end) {
-
         if (start < end) {
             int p = partition(arr, start, end);
-            sort(arr, start, p); // hoare分区
-            //sort(arr, start, p - 1) ; // lomuto分区
+            sort(arr, start, p); // hoare分区，p并不是基准元素在新数组中的位置
+            //sort(arr, start, p - 1) ; // lomuto分区，p是基准元素在新数组中的位置，因此下一次可以不包括它
             sort(arr, p + 1, end);
         }
     }
 
     private int partition(int[] arr, int start, int end) {
-        int pivot = arr[start];
+        int pivot = arr[start]; // hoare分区，总是以第一个为基准
         int i = start - 1;
         int j = end + 1;
         while (true) {
@@ -28,7 +27,7 @@ public class QSort {
             }
         }
 
-        //int pivot = arr[end]; //lomuto分区
+        //int pivot = arr[end]; // lomuto分区，总是以最后一个为基准
         //int i = start;
         //for (int j = start; j < end ; j++) {
         //    if (arr[j] <= pivot) {
